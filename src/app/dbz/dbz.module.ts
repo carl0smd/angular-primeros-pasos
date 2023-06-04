a1ea4d65b670a666d4d6795538612990c308ed78
Carlos Martín <carlos.martindu@gmail.com>
2023-06-04 11:35:53 +0100

    first commit

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CharacterComponent } from './components/character/character.component';
+import { ListComponent } from './components/list/list.component';
+import { MainPageComponent } from './pages/main-page.component';
+
+@NgModule({
+  declarations: [MainPageComponent, ListComponent, CharacterComponent],
+  exports: [MainPageComponent],
+  imports: [CommonModule, FormsModule],
+})
+export class DbzModule {}
